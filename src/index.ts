3b8a3c68117ad8c497a9fbd97b2603d793375ee3
vv-01 <asvijayavitthala@gmail.com>
2024-02-18 05:17:09 +0530

    added new route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { fetch } from "undici";
+import * as fs from "fs";
 
 const app = new Hono();
 
@@ -13,12 +14,15 @@ app.get("/benchmark", async (c) => {
   const data = await fetch(
     "https://json.bselivefeeds.indiatimes.com/marketband.json"
   );
-  const NiftyData = await fetch("https://json.bselivefeeds.indiatimes.com/ET_Community/liveindices?outputtype=json&indexid=1913&exchange=50&company=true&pagesize=500&sortby=percentchange&sortorder=desc&language=&callback=objIndices.getDataCB");
+  const NiftyData = await fetch(
+    "https://json.bselivefeeds.indiatimes.com/ET_Community/liveindices?outputtype=json&indexid=1913&exchange=50&company=true&pagesize=500&sortby=percentchange&sortorder=desc&language=&callback=objIndices.getDataCB"
+  );
   const textData = (await data.text()).trim().slice(15, -1);
   const JSONData = JSON.parse(textData);
   const NifyTextData = (await NiftyData.text()).trim().slice(21, -1);
   const NiftyJSONData = JSON.parse(NifyTextData);
-  return c.json([JSONData.marketBandList
+  return c.json([
+    JSONData.marketBandList
       .map((item: any) => item.bandServiceMetas)[0]
       .map((data: any) => {
         return [
@@ -31,8 +35,13 @@ app.get("/benchmark", async (c) => {
           },
         ];
       }),
-      Number(NiftyJSONData.searchresult.index.indexvalue.lastvalue)]
-  );
+    Number(NiftyJSONData.searchresult.index.indexvalue.lastvalue),
+  ]);
+});
+
+app.get("/stockDetails", async (c) => {
+  const data = await fs.promises.readFile(`src/stockDetails.json`);
+  return c.json(JSON.parse(data.toString()));
 });
 
 const port = 3000;
