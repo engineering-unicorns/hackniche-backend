5be7f12138541928f48be8c6747691a1d6b32be2
anishshobithps <anish.shobith19@gmail.com>
2024-02-17 23:55:46 +0530

    feat: benchmark data

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { fetch } from "undici";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+// /benchmark
+app.get("/benchmark", async (c) => {
+  const data = await fetch(
+    "https://json.bselivefeeds.indiatimes.com/marketband.json"
+  );
+  const NiftyData = await fetch("https://json.bselivefeeds.indiatimes.com/ET_Community/liveindices?outputtype=json&indexid=1913&exchange=50&company=true&pagesize=500&sortby=percentchange&sortorder=desc&language=&callback=objIndices.getDataCB");
+  const textData = (await data.text()).trim().slice(15, -1);
+  const JSONData = JSON.parse(textData);
+  const NifyTextData = (await NiftyData.text()).trim().slice(21, -1);
+  const NiftyJSONData = JSON.parse(NifyTextData);
+  return c.json([JSONData.marketBandList
+      .map((item: any) => item.bandServiceMetas)[0]
+      .map((data: any) => {
+        return [
+          {
+            name: data.serviceName,
+            open: data.open,
+            high: data.high,
+            low: data.low,
+            current: data.current,
+          },
+        ];
+      }),
+      Number(NiftyJSONData.searchresult.index.indexvalue.lastvalue)]
+  );
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
